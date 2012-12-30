afcf98014f73bb5aea6e100d5bc1d448d4c2130e
Vegard Hanssen <Vegard.Hanssen@menneske.no>
2012-12-30 23:12:49 +0100

    changed init to addLanguage and added addToLanguage. Added tests of wordlists

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# wordlist
+
 Node.js library to check words in different languages.
 
 You can do something like this
@@ -14,5 +16,38 @@ into the directory lib/wordlists/words_&lt;lang&gt;.txt and contains a word for
 each line. One way of making wordlists is using the wordlists from aspell.
 You can generate the wordlists from aspell like this:
 
-	aspell -d en dump master &gt; lib/wordlists/words_en.txt
+	aspell -d en dump master > lib/wordlists/words_en.txt
 
+## addLanguage(lang, directory, callback)
+## addLanguage(lang, callback)
+
+This function will read a wordlist from file to be used. It will organize it
+in memory for usage. You can have many languages in memory at the same time.
+
+**directory** is optional and will use the lib/wordlists/ as default. A good
+idea is to keep this outside of the module.
+
+## addToLanguage(lang, to_lang, directory, callback)
+## addToLanguage(lang, to_lang, callback)
+
+This function will read a wordlist from a file to be added to another language.
+Eg, you can do something like
+
+	wordlist.addLanguage('en'..);
+	wordlist.addToLanguage('en_GB', 'en'..);
+	wordlist.addToLanguage('en_US', 'en'..);
+
+And now you have all the GB and US words in the same internal "en" language.
+
+## checkWord(lang, word)
+
+This will check a word in a language and return true/false
+
+## addWord(lang, word)
+
+This will add a word to a language and return true if added or false if not.
+It will return false if the word already exists.
+
+## wordCound(lang)
+
+Return the count of words in a language
--- test/test_wordlist.js
+++ test/test_wordlist.js
@@ -7,23 +7,57 @@ require("assert");
 var wordlist = require('../lib/wordlist'); 
 
 describe('wordlist', function() {
-	describe('#init()', function() {
-		it('should return on init language norwegian', function(done) {
-			wordlist.init('no', function() {
+	describe('#addLanguage()', function() {
+		it('should return on adding language norwegian', function(done) {
+			wordlist.addLanguage('no', function() {
 				done();
 			});
+		});
+		it('should return on adding language english', function(done) {
+			wordlist.addLanguage('en', function() {
+				done();
+			});
+		});
+		it('should read language english when loading from other directory', function(done) {
+			wordlist.addLanguage('en', 'test/wordlists/', function() {
+				done();
+			});
+		});
+		it('should have removed the old words from english when loading from other directory', function() {
+			wordlist.checkWord('en', 'test').should.be.false;
+			wordlist.checkWord('en', 'testing').should.be.true;
+		});
+	});
+	
+	describe('#addToLanguage()', function() {
+		it('should add to language en from en_GB', function(done) {
+			wordlist.addToLanguage('en_GB', 'en', 'test/wordlists/', function() {
+				done();
+			});
+		});
+		it('should have the new word tester in en', function() {
+			wordlist.checkWord('en', 'tester').should.be.true;
+		});
+		it('should have the old word testing in en', function() {
+			wordlist.checkWord('en', 'testing').should.be.true;
 		});
 	});
 	
 	describe('#wordCount()', function() {
 		it('should have populated words', function() {
-			wordlist.wordCount().should.be.above(2);
+			wordlist.wordCount().should.equal(5);
 		});
 		it('should return -1 on unknown language', function() {
 			wordlist.wordCount('dummy').should.equal(-1);
 		});
 		it('should return 2 on language test', function() {
 			wordlist.wordCount('test').should.equal(2);
+		});
+		it('should return 1 on language no', function() {
+			wordlist.wordCount('no').should.equal(1);
+		});
+		it('should return 2 on language en', function() {
+			wordlist.wordCount('en').should.equal(2);
 		});
 	});
 
@@ -49,8 +83,14 @@ describe('wordlist', function() {
 		it('should not know the word BFHEJF in language no', function() {
 			wordlist.checkWord('no', 'BFHEJF').should.be.false;
 		})
+		it('should know the word TESTING in language en', function() {
+			wordlist.checkWord('en', 'testing').should.be.true;
+		});
+		it('should not know the word FRIGJØRING in language en', function() {
+			wordlist.checkWord('en', 'FRIGJØRING').should.be.false;
+		});
 	});
-	
+
 	describe('#addWord()', function() {
 		it('should return true when adding word testing to language test', function() {
 			wordlist.addWord('test', 'testing').should.be.true;
